eef36b98e32094cffcd3739df63a8bcf72d8c4c1
meztinosdunken <orlanmorry5462@hotmail.com>
2025-08-16 11:13:40 -0500

    Add MongoDB connection error handling

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const io = new Server(server);
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/iteration-planning', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Iteration Planning Tool API');
